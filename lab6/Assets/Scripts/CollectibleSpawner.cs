278fffdfb0c559dfd82b93ba6e51b4532a430d6d
Atthawit <87508680+atthawit-ratakaset@users.noreply.github.com>
2022-10-12 23:59:08 +0700

    Lab6

---
+++ lab6/Assets/Scripts/CollectibleSpawner.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class CollectibleSpawner : MonoBehaviour
+{   
+    public ParticleSystem collect;
+    public ParticleSystem respawn;
+    // This script is to handle the respawning of the collectible as a disabled gameObject cannot run any methods or coroutines on its own.
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject collectibleGameObject;
+    [SerializeField] private AudioPlayer audioPlayer;
+    [SerializeField] private SoAudioClips collectAudioClips;
+    [SerializeField] private SoAudioClips respawnAudioClips;
+    
+    [Header("Collectible Settings")]
+    [SerializeField] private float respawnTime = 4f;
+
+    private void Start()
+    {
+        spriteRenderer.enabled = false;
+    }
+    
+    private IEnumerator RespawnCollectible()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        SetOutlineSpriteActive(false);
+        PlayRespawn();
+        collectibleGameObject.SetActive(true);
+    }
+
+    private void SetOutlineSpriteActive(bool state)
+    {
+        spriteRenderer.enabled = state;
+    }
+
+    public void SetOutlineSprite(Sprite sprite)
+    {
+        spriteRenderer.sprite = sprite;
+    }
+    
+    public void StartRespawningCountdown() // This method is to let other script trigger the respawn countdown, and let this script handle the coroutine.
+    {
+        SetOutlineSpriteActive(true);
+        PlayCollected();
+        StartCoroutine(RespawnCollectible());
+    }
+    
+    #region Audio
+    private void PlayRespawn()
+    {   
+        CraeteRespawn();
+        audioPlayer.PlaySound(respawnAudioClips);
+    }
+
+    private void PlayCollected()
+    {
+        CreateCollect();
+        audioPlayer.PlaySound(collectAudioClips);
+    }
+
+    private void CreateCollect()
+    {
+        collect.Play();
+    }
+
+    private void CraeteRespawn()
+    {
+        respawn.Play();
+    }
+    #endregion
+}
---
+++ lab6/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{   
+    public ParticleSystem win;
+    [SerializeField] private SoAudioClips winAudioClips;
+    [SerializeField] private AudioPlayer audioPlayer;
+    
+    private const string PlayerTag = "Player";
+
+    private GameManager _gameManager;
+
+    private bool _triggeredNextScene;
+    
+    // Why are we checking if the player reaches the finish line here? So, we do not
+    // have to check for every time the player collides with something for a finish line.
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.CompareTag(PlayerTag)) return;
+
+        if (_triggeredNextScene) return;
+
+        _triggeredNextScene = true;   // This is to prevent double scene loads.
+        _gameManager = FindObjectOfType<GameManager>();
+        audioPlayer.PlaySound(winAudioClips);
+        CreateWin();
+        _gameManager.LoadNextLevel();
+    }
+
+    private void CreateWin()
+    {
+        win.Play();
+    }
+}
